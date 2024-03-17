9e76ad8dd9e651bc853a72d8617b53219f6fe504
Osajele Odegua <Odeguaj@gmailcom>
2024-03-17 14:42:02 +0100

    frame work for my web with react.js

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-centter py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">FoodRecipe</h2>
+    </nav>
+  );
+}
